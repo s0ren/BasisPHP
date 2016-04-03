27d3785e3107be9b5d55d7378f5d187243b0e727
Søren Magnusson <soren@magnussons.dk>
2016-04-04 01:00:41 +0200

    HTML skelet til løsningen

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Temperaturtabel</title>
+    </head>
+    <body>
+        <table>
+            <th>
+            <td>
+                Fahrenheit
+            </td>
+            <td>
+                Celsius
+            </td>
+            </th>
+            <?php
+         
+            
+            ?>
+        </table>
+    </body>
+</html>
